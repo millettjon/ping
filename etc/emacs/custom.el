beb2c067cd1a7e5fd332898fba25d9969b730534
Jonathan Millett <jon@ewaydirect.com>
2016-12-14 09:57:55 -0500

    emacs - Use smartparens in strict mode.

--- etc/emacs/custom.el
+++ etc/emacs/custom.el
@@ -1,7 +1,14 @@
+;; My packages
+(setq prelude-packages (append '(
+                                 ; graphviz-dot-mode
+                                 ) prelude-packages))
+;; Install my packages
+(prelude-install-packages)
+
 ;; Disable a few things that are too intrusive.
 (setq prelude-whitespace nil)
 (setq prelude-clean-whitespace-on-save nil)
 
-;; Setup paredit.
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+;; smartparens - Use strict mode to act more like paredit.
+(add-hook 'emacs-lisp-mode-hook       #'smartparens-strict-mode)
+(add-hook 'clojure-mode-hook          #'smartparens-strict-mode)
